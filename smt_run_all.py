8dee6ec438a2c6fe6e76d70b0511d0f764a7a29e
zhangyushan <zhangysh1995@gmail.com>
2017-08-07 18:32:34 +0800

    configures

--- smt_run_all.py
+++ smt_run_all.py
@@ -2,16 +2,16 @@ import os
 import pwd
 import argparse
 import smt_tests as st
-from tools.ssh import scp_csv
+# from tools.ssh import scp_csv
 
 
-def split_project(entrance):
+def split_project(entrance, cpus):
 	if os.path.exists(entrance):
 		for path, dir, file in os.walk(entrance):
 			if len(dir) != 0:
 				dir = [os.path.join(path, i) for i in dir]
 				while len(dir) > 0:
-					split_project(dir.pop())
+					split_project(dir.pop(), cpus)
 			else:
 				print('\n\n===== Tests under folder ' + path + ' =====')
 				file = [os.path.join(path, f) for f in file if f.split('.')[1] == 'smt2']
@@ -21,21 +21,24 @@ def split_project(entrance):
 		print('Directory Missing ' + entrance)
 
 parser = argparse.ArgumentParser(description='Run all your smt2 cases via all solvers.')
+parser.add_argument('--cconfig', type=str, help='point to your configuration files (e.g config.)')
 parser.add_argument('--case', type=str, help='absolute path to your test cases root directory')
 parser.add_argument('--cpus', type=int, help='how many cpus cores you want to use? (Better under 24)')
 args = parser.parse_args()
 
-if args.cpus == None:
+
+cpus = 0
+if args.cpus is None:
 	pass
 else:
 	cpus = args.cpus
 
-if args.case == None:
+if args.case is None:
 	if pwd.getpwuid(os.geteuid()).pw_name == 'zhangysh1995':
-		split_project('/home/zhangysh1995/ctags')
+		split_project('/home/zhangysh1995/ctags', cpus)
 	elif pwd.getpwuid(os.geteuid()).pw_name == 'root':
-		split_project('/root/PPBV')
+		split_project('/root/PPBV', cpus)
 else:
-	split_project(args.case)
+		split_project(args.case, cpus)
 
-scp_csv()
+# scp_csv()
