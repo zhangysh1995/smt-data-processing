5d8a7635d78bc78351bf09345c1784746765fbbf
zhangyushan <zhangysh1995@gmail.com>
2017-08-08 17:34:59 +0800

    update

--- smt_run_all.py
+++ smt_run_all.py
@@ -25,52 +25,55 @@ def split_project(entrance, factory, cpus):
 	else:
 		print('Directory Missing ' + entrance)
 
-parser = argparse.ArgumentParser(description='Run all your smt2 cases via all solvers.')
-parser.add_argument('--config', type=str, help='point to your configuration files (e.g config.)')
-parser.add_argument('--case', type=str, help='absolute path to your test cases root directory')
-parser.add_argument('--cpus', type=int, help='how many cpus cores you want to use? (Better under 24)')
-args = parser.parse_args()
 
-
-cpus = 0
-solver_path = []
-
-if args.config is None:
-	if username() == 'zhangysh1995':  # configure on my local desktop
-		z3_path = '/home/zhangysh1995/work/ppdev/z3/build/z3'
-		stp_path = '/home/zhangysh1995/work/stp/stp/build/stp-2.1.2 --SMTLIB2'
-		pp_path = '/home/zhangysh1995/work/ppdev/ppsat/ppbv'
-		boolector_path = '/home/zhangysh1995/work/boolector-2.4.1/boolector/bin/boolector --smt2'
-	else:   # configure on sbtest1 docker image
-		z3_path = '/root/Solvers/z3-4.5.0/build/z3'
-		stp_path = '/root/Solvers/stp/stp/build/stp-2.1.2'
-		pp_path = '/root/Solvers/ppsat-array/build-dev/ppbv'
-		boolector_path = '/root/Solvers/boolector-2.4.1/boolector/bin/boolector --smt2'
-	solver_path = [z3_path, stp_path, pp_path, boolector_path]
-else:
+def configure(args):
 	config_file = args.config
 	config = configparser.ConfigParser()
-	config.read(config_file)
-	z3_path = config.get('z3', 'path')
-	stp_path = config.get('stp', 'path')
-	pp_path = config.get('ppbv', 'path')
-	boolector_path = config.get('boolector', 'path')
-	solver_path = [z3_path, stp_path, pp_path, boolector_path]
-	print(solver_path)
 
-if args.cpus is None:
-	pass
-else:
-	cpus = args.cpus
-
-factory = SolverFactory(solver_path)
-
-if args.case is None:
-	if username() == 'zhangysh1995':
-		split_project('/home/zhangysh1995/ctags', factory, cpus)
+	if args.config is None:
+		print('====== Use -h for help if you want configuration file ======')
+		if username() == 'zhangysh1995':  # configure on my local desktop
+			z3_path = '/home/zhangysh1995/work/ppdev/z3/build/z3'
+			stp_path = '/home/zhangysh1995/work/stp/stp/build/stp-2.1.2 --SMTLIB2'
+			pp_path = '/home/zhangysh1995/work/ppdev/ppsat/ppbv'
+			boolector_path = '/home/zhangysh1995/work/boolector-2.4.1/boolector/bin/boolector --smt2'
+		else:  # configure on sbtest1 docker image
+			z3_path = '/root/Solvers/z3-4.5.0/build/z3'
+			stp_path = '/root/Solvers/stp/stp/build/stp-2.1.2'
+			pp_path = '/root/Solvers/ppsat-array/build-dev/ppbv'
+			boolector_path = '/root/Solvers/boolector-2.4.1/boolector/bin/boolector --smt2'
+		solver_path = [z3_path, stp_path, pp_path, boolector_path]
+		factory = SolverFactory(solver_path)
+		cases = input('Path to your cases: ')
+		cpus = input('CPU cores: ')
+		split_project(cases, factory, cpus=int(cpus))
 	else:
-		split_project('/root/PPBV', factory, cpus)
-else:
-		split_project(args.case, factory, cpus)
+		with open(config_file, 'r') as f:
+			for line in f:
+				print(line)
+			f.close()
 
+		read = input('Continue?(y/n) ')
+		if read == 'n':
+			exit(0)
+
+		config.read(config_file)
+		z3_path = config.get('z3', 'path')
+		stp_path = config.get('stp', 'path')
+		pp_path = config.get('ppbv', 'path')
+		boolector_path = config.get('boolector', 'path')
+		solver_path = [z3_path, stp_path, pp_path, boolector_path]
+
+		factory = SolverFactory(solver_path)
+		split_project(config.get('general', 'cases'), factory, cpus=int(config.get('general', 'cpus')))
+
+
+def run():
+	parser = argparse.ArgumentParser(description='Run all your smt2 cases via all solvers.')
+	parser.add_argument('--config', type=str, help='point to your configuration files (e.g config.)')
+	args = parser.parse_args()
+	configure(args)
+
+
+run()
 # scp_csv()
