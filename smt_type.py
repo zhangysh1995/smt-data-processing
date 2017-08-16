e124b6f3bd62579e781274728e3f2f891b935be0
zhangyushan <zhangysh1995@gmail.com>
2017-08-16 10:58:55 +0800

    distribution of var in queries

--- smt_type.py
+++ smt_type.py
@@ -2,11 +2,10 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
-
+import matplotlib.ticker as mtick
 smtbench = '/home/zhangysh1995/work/ppdev/smtbench/cmake-build-debug/smtbench '
 
-
+colors = ['orchid', 'dodgerblue', 'springgreen', 'yellow']
 # query type of a project
 def type_dir(project):
 	for path, dirs, files in os.walk(project):
@@ -81,6 +80,45 @@ def variable_hist(file):
 	return dir, var_list
 
 
+# draw distribution of variables for one file
+def var_percent(file):
+	var = variable_hist(file)
+	dir = var[0]
+	var_list = var[1]
+
+	ax = plt.subplot()
+	# generate x position for bars
+	ticks = range(len(dir)+1)[1:]
+	# iterate each project
+	for d, v, x in zip(dir, var_list, ticks):
+		window = np.linspace(0, max(v), 4)
+		count = []
+		all = len(v)
+		# count cumulative sum with window
+		for w in window:
+			count.append(len(list(filter(lambda n: n <= w, v))))
+		# normalize y
+		count = [c/all*100 for c in count]
+		# plot the bar for one project stacked
+		for i, c in enumerate(count):
+			if i == 0:
+				plt.bar(x, c/all*100, color=colors[i])
+			else:
+				plt.bar(x, c-count[i-1], bottom=count[i-1], color=colors[i])
+	# format y tick to percentage
+	fmt = '%.0f%%'
+	yticks = mtick.FormatStrFormatter(fmt)
+	ax.yaxis.set_major_formatter(yticks)
+	# set x ticks
+	plt.xticks(ticks, dir)
+	# set legend
+	plt.title('Distribution of Variable Numbers Across sage Projects')
+	plt.xlabel('project')
+	plt.ylabel('No.')
+
+# def feature_hist(file):
+
+
 '''
 Below are function wrappers
 '''
@@ -88,15 +126,16 @@ Below are function wrappers
 
 def draw_variable_hist(file):
 	var = variable_hist(file)
-	var_list = var[1]
 	dir = var[0]
+	var_list = var[1]
 
 	for i, v in enumerate(var_list):
 		pd.DataFrame(v).plot(kind='hist', title=dir[i])
 
 
-type_dir('/home/zhangysh1995/PPBV/PP-CASE')
+# type_dir('/home/zhangysh1995/PPBV/PP-CASE')
 # draw_type ('/home/zhangysh1995/PPBV/KLEE')
 # plt.bar(range(0, 32), count('sage'))
 # variable_hist('sage')
-# plt.show()
+# var_percent('sage')
+plt.show()
