ad6946a909ad4be6fde6e18987a363408e699815
zhangyushan <zhangysh1995@gmail.com>
2017-08-21 18:53:15 +0800

    zoom-out plot

--- smt_analyze.py
+++ smt_analyze.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tools import brokenaxes as bka
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 '''
 Usage:
@@ -22,7 +25,8 @@ Below are function definitions
 
 # solvers = ['s1', 's2', 's3']
 solvers = ['s0', 's1', 's2', 's3', 's4', 's5', 's6']
-name = ['$TP_{none}$', '$TP_{light}$', '$TP_{heavy}$']
+# name = ['$TP_{none}$', '$TP_{light}$', '$TP_{heavy}$']
+name = ['$Baseline$', '$RSUB$', '$ELS$', '$FLE$', '$BCE$', '$RSUB + ELS$', '$ALL$']
 colors =['orange', 'dodgerblue', 'limegreen', 'pink', 'gray', 'purple', 'brown']
 markers = ['d', 's', 'x', '+', 'o', '^', 'p']
 
@@ -56,16 +60,37 @@ def time_sovled(file):
 	rows = len(times[1])
 	Xticks =[int(x) for x in np.linspace(0, rows, 51)]
 
+	zoom = []
+
+	fig, ax = plt.subplots()
+	# bax = bka.brokenaxes(xlims=((0, int(rows/5)), (rows-2000, 7000)), hspace=.05)
 	for i, time in enumerate(times):
 		cumsum = []
 		for tick in Xticks:
 			cumsum.append(sum(time[:int(tick)]))
 		plt.xlim(0, rows+500)
-		plt.plot(Xticks, cumsum, label=solvers[i], color=colors[i], marker=markers[i], markersize=5)
+
+		ax.plot(Xticks, cumsum, label=name[i], color=colors[i], marker=markers[i], markersize=5)
+		zoom.append(cumsum[-5:])
 
 	plt.xlabel('Solved instances')
 	plt.ylabel('Cumulative running time/s')
-	plt.legend()
+	plt.legend(loc='lower right')
+
+	# draw zoom-in plot
+	axins = zoomed_inset_axes(ax, 2.3, loc=2)
+	axins.set_xlim(6000, 7000)
+	axins.set_ylim(65, 95)
+	plt.xticks(visible=False)
+	plt.yticks(visible=False)
+	axins.xaxis.set_visible(False)
+	axins.yaxis.set_visible(False)
+	mark_inset(ax, axins, loc1=2, loc2=4, fc="none", ec='gray')
+	for i, t in enumerate(zoom):
+		axins.plot(Xticks[-5:], zoom[i], color=colors[i], marker=markers[i])
+	# axins.plot()
+
+
 	plt.savefig('pre-processing.png', bbox_inches='tight')
 
 
